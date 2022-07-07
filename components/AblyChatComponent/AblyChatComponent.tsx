77af2f4c51a4626ef78501ad516547ac7f14354f
soju <underrated@gmail.com>
2022-07-07 15:31:34 -0700

    add tailwind styling

--- components/AblyChatComponent/AblyChatComponent.tsx
+++ components/AblyChatComponent/AblyChatComponent.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useChannel } from '../../hooks/AblyReactEffect';
 import styles from './AblyChatComponent.module.css';
-import { Input } from '@chakra-ui/react';
+
 const AblyChatComponent = () => {
     let inputBox: any = null;
     let messageEnd: any = null;
@@ -9,7 +9,6 @@ const AblyChatComponent = () => {
     const [messageText, setMessageText] = useState('');
     const [receivedMessages, setMessages] = useState<any[]>([]);
     const messageTextIsEmpty = messageText.trim().length === 0;
-
 
     const [channel, ably] = useChannel('chat-demo', (message) => {
         // Here we're computing the state that'll be drawn into the message history
@@ -23,74 +22,70 @@ const AblyChatComponent = () => {
         // setMessages react useState hook
     });
 
-  const sendChatMessage = (messageText: string) => {
-    //@ts-ignore
-    console.log(process.env.NEXT_APP_ABLY_API_KEY);
-    channel.publish({ name: "chat-message", data: messageText });
-    setMessageText("");
-    inputBox.focus();
-  };
+    const sendChatMessage = (messageText: string) => {
+        //@ts-ignore
+        console.log(process.env.NEXT_APP_ABLY_API_KEY);
+        channel.publish({ name: 'chat-message', data: messageText });
+        setMessageText('');
+        inputBox.focus();
+    };
 
-  const handleFormSubmission = (event: any) => {
-    event.preventDefault();
-    sendChatMessage(messageText);
-  };
+    const handleFormSubmission = (event: any) => {
+        event.preventDefault();
+        sendChatMessage(messageText);
+    };
 
-  const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
-    if (event.charCode !== 13 || messageTextIsEmpty) {
-      return;
-    }
-    sendChatMessage(messageText);
-    event.preventDefault();
-  };
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLDivElement>) => {
+        if (event.charCode !== 13 || messageTextIsEmpty) {
+            return;
+        }
+        sendChatMessage(messageText);
+        event.preventDefault();
+    };
 
-  const messages = receivedMessages.map((message, index) => {
-    //@ts-ignore
-    const author = message.connectionId === ably.connection.id ? "me" : "other";
+    const messages = receivedMessages.map((message, index) => {
+        //@ts-ignore
+        const author = message.connectionId === ably.connection.id ? 'me' : 'other';
+        return (
+            <span key={index} className={styles.message} data-author={author}>
+                {message.data}
+            </span>
+        );
+    });
+
+    useEffect(() => {
+        messageEnd.scrollIntoView({ behaviour: 'smooth' });
+    });
+
     return (
-      <span key={index} className={styles.message} data-author={author}>
-        {message.data}
-      </span>
-    );
-  });
-
-  useEffect(() => {
-    messageEnd.scrollIntoView({ behaviour: "smooth" });
-  });
-
-  return (
-    <div className={styles.chatWrapper}>
-      <div className={styles.chatHolder}>
-        <div className={styles.chatText}>
-          {messages}
-          <div
-            ref={(element) => {
-              messageEnd = element;
-            }}
-          ></div>
+        <div className="flex h-full w-full flex-col">
+            <div className="mb-4 h-96 w-full rounded-md bg-stone-500">
+                <div className="flex h-full flex-col items-start">
+                    {messages}
+                    <div
+                        ref={(element) => {
+                            messageEnd = element;
+                        }}
+                    ></div>
+                </div>
+            </div>
+            <form onSubmit={handleFormSubmission} className="flex w-full flex-row space-x-4">
+                <input
+                    ref={(element) => {
+                        inputBox = element;
+                    }}
+                    value={messageText}
+                    placeholder="Type a message..."
+                    onChange={(e) => setMessageText(e.target.value)}
+                    onKeyDown={(e) => handleKeyPress(e)}
+                    className="w-full rounded-md bg-stone-500 px-2 py-1"
+                ></input>
+                <button type="submit" className="rounded-md bg-stone-500 px-2 py-1" disabled={messageTextIsEmpty}>
+                    Send
+                </button>
+            </form>
         </div>
-      </div>
-      <form onSubmit={handleFormSubmission} className={styles.form}>
-        <input
-          ref={(element) => {
-            inputBox = element;
-          }}
-          value={messageText}
-          placeholder="Type a message..."
-          onChange={(e) => setMessageText(e.target.value)}
-          onKeyDown={(e) => handleKeyPress(e)}
-          className={styles.textarea}
-        ></input>
-        <button
-          type="submit"
-          className={styles.button}
-          disabled={messageTextIsEmpty}
-        >
-          Send
-        </button>
-      </form>
-    </div>
-  );
+    );
 };
 
 export default AblyChatComponent;
